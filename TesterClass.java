2b94cf9879a734d8f9be2622cfbe92c3f57d6c34
ibarhim <46403831+Phospheness@users.noreply.github.com>
2024-03-06 14:58:11 +0000

    Create TesterClass.java
    first draft of tester class

---
+++ TesterClass.java
@@ -0,0 +1,28 @@
+package walle;
+import lejos.hardware.Button;
+
+public class TesterClass {
+  
+	public void testRotate(){
+		Rotate Rotate = new Rotate();
+		UVSensor uvSensor = new UVSensor();
+		while (Button.ENTER.isDown()) {
+			Rotate.stopRotate();                 //move to emergency  stop behaviour
+		}
+		Rotate.rotateSensor(200, 50, 360);
+		while (Rotate.getState() == true) {
+			float Distance = uvSensor.getDistance();
+			if (Distance < 0.5f) {
+				;
+			} else if (Distance >= 0.5f && Distance < 1.0f) {
+				;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		TesterClass test = new TesterClass();
+		test.testRotate();
+	}
+
+}
