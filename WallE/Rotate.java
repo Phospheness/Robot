775946ae07a8ee3e6d52ba84eac51041a5304af2
ibarhim <46403831+Phospheness@users.noreply.github.com>
2024-03-06 16:06:45 +0000

    Update Rotate.java
    moves based ondirection instead of angle

--- WallE/Rotate.java
+++ WallE/Rotate.java
@@ -7,39 +7,65 @@ import lejos.hardware.port.MotorPort;
 public class Rotate {
 	private static EV3MediumRegulatedMotor spinMotor = new EV3MediumRegulatedMotor(MotorPort.A);
 	private static boolean isRotating = false;
+
+	public void rotateSensor(int delay, int speed) {
+	    if (!isRotating) {
+	        isRotating = true;
+	        String direction = "left";
+	        while (isRotating) {
+	            // Rotate left
+	            rotateDirection("left", 90, speed);
+	            applyDelay(delay);
+	            
+	            // Rotate right to original position
+	            rotateDirection("right", 180, speed);
+	            applyDelay(delay);
+	            
+	            // Toggle direction
+	            direction = (direction.equalsIgnoreCase("left")) ? "right" : "left";
+	        }
+	    } else {
+	        LCD.drawString("Motor is already rotating", 2, 2);
+	    }   
+	}
+
+	/*
+	public void rotateSensor(int degrees, int delay, int speed) {
+	    if (!isRotating) {
+	        isRotating = true;
+	        String direction = "left";
+	        while (isRotating) {
+	            rotateDirection(direction, degrees, speed); // Rotate in the current direction
+	            applyDelay(delay); // Apply delay before changing direction
+	            
+	            // Toggle direction
+	            direction = (direction.equalsIgnoreCase("left")) ? "right" : "left";
+	            rotateDirection(direction, degrees, speed); //reset to original position
+	        }
+	    } else {
+	        LCD.drawString("Motor is already rotating", 2, 2);
+	    }   
+	}
+	*/
+
+
+	public void rotateDirection(String direction, int degrees, int speed) {
+	    if (direction.equalsIgnoreCase("left")) {
+	        spinMotor.rotate(degrees, true); 
+	    } else if (direction.equalsIgnoreCase("right")) {
+	        spinMotor.rotate(-degrees, true); 
+	    } else {
+	        LCD.drawString("Not a valid direction", 0, 0);
+	    }
+	}
 	
-	public void rotateSensor(int degrees, int delay,int speed) {
-		if (!isRotating) {
-			isRotating = true;
-			spinMotor.setSpeed(speed);
-			spinMotor.forward();
-			while (spinMotor.getTachoCount() < degrees) {
-				Thread.yield();
-			}
-			spinMotor.stop();
-			spinMotor.resetTachoCount();
 	
-			try {
-				Thread.sleep(delay);
-			} catch (InterruptedException error) {
-				error.printStackTrace();
-			}
-			
-			spinMotor.backward();
-			while (spinMotor.getTachoCount() > -degrees) {
-				Thread.yield();
-			}
-			spinMotor.stop();
-			spinMotor.resetTachoCount();
-			
-			try {
-				Thread.sleep(delay);
-			} catch (InterruptedException error) {
-				error.printStackTrace();
-			} 
-		} else {
-			LCD.drawString("Motor is already rotating", 2, 2);
-		}	
+	public void applyDelay(int delay) {       
+	    try {
+	        Thread.sleep(delay);
+	    } catch (InterruptedException error) {
+	        error.printStackTrace();
+	    }
 	}
 	
 	public void stopRotate() {
