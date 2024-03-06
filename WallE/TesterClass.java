87003dfdb5fde5901c1b146aebe7decad591fba0
ibarhim <46403831+Phospheness@users.noreply.github.com>
2024-03-06 16:06:16 +0000

    Update TesterClass.java
    updated to work with rotate

--- WallE/TesterClass.java
+++ WallE/TesterClass.java
@@ -1,15 +1,16 @@
-package walle;
+package walle;                                       
+
 import lejos.hardware.Button;
 
 public class TesterClass {
-  
+	
 	public void testRotate(){
 		Rotate Rotate = new Rotate();
 		UVSensor uvSensor = new UVSensor();
 		while (Button.ENTER.isDown()) {
 			Rotate.stopRotate();                 //move to emergency  stop behaviour
 		}
-		Rotate.rotateSensor(200, 50, 360);
+		Rotate.rotateSensor(50, 90);
 		while (Rotate.getState() == true) {
 			float Distance = uvSensor.getDistance();
 			if (Distance < 0.5f) {
