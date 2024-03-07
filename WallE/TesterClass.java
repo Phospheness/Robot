7f62da1adfe158f5ce6f9328cbe2a498134c61f0
ibarhim <46403831+Phospheness@users.noreply.github.com>
2024-03-07 15:01:16 +0000

    Update TesterClass.java
    removing thread methods to arbitrators

--- WallE/TesterClass.java
+++ WallE/TesterClass.java
@@ -1,30 +1,60 @@
-package walle;                                       
-import lejos.hardware.Button;
+package walle;
 
-public class TesterClass {
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.utility.*;
+
+public class Rotate {
+	private static EV3MediumRegulatedMotor spinMotor = new EV3MediumRegulatedMotor(MotorPort.A);
+	private static boolean isRotating = false;
+
+	public void rotateSensor(int delay, int speed) {
+	    if (isRotating == false) {
+	        isRotating = true;
+	        String direction = "left";
+	        while (isRotating == true) {
+	            // Rotate left
+	            rotateDirection("left", 90, speed);
+	            applyDelay(delay);
+	            
+	            // Rotate right to original position
+	            rotateDirection("right", 180, speed);
+	            applyDelay(delay);
+	            
+	            // Toggle direction
+	            direction = (direction.equalsIgnoreCase("left")) ? "right" : "left";
+	        }
+	    } else {
+	        LCD.drawString("Motor is already rotating", 2, 2);
+	    }   
+	}
+
+	public void rotateDirection(String direction, int degrees, int speed) {
+	    if (direction.equalsIgnoreCase("left")) {
+	        spinMotor.rotate(degrees, true); 
+	    } else if (direction.equalsIgnoreCase("right")) {
+	        spinMotor.rotate(-degrees, true); 
+	    } else {
+	        LCD.drawString("Not a valid direction", 0, 0);
+	    }
+	}
 	
-	public void testRotate(){
-		Rotate Rotate = new Rotate();
-		UVSensor uvSensor = new UVSensor();
-		
-		while (Button.ENTER.isDown()) {
-			Rotate.stopRotate();                 //move to emergency  stop behaviour
-		}
-		
-		Rotate.rotateSensor(50, 90);
-		while (Rotate.getState() == true) {
-			float Distance = uvSensor.getDistance();
-			if (Distance < 0.5f) {
-				;
-			} else if (Distance >= 0.5f && Distance < 1.0f) {
-				;
-			}
+	
+	public void applyDelay(int delay) {
+        Delay.msDelay(delay);
+    }
+	
+	public void stopRotate() {
+		if (isRotating == true) {
+			isRotating = false;
+			spinMotor.stop();
+		} else {
+			LCD.drawString("Motor is not rotating", 2, 2);
 		}
 	}
-
-	public static void main(String[] args) {
-		TesterClass test = new TesterClass();
-		test.testRotate();
+	
+	public boolean getState() {
+		return isRotating;
 	}
-
 }
