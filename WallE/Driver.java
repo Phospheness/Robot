e3636535ca13436b191982750c6b61f0b3d9b655
Laijae <laijae@hotmail.com>
2024-03-13 21:46:27 +0000

    Implemented MB's Driver and NodeDetector files, few tweaks and additions

---
+++ WallE/Driver.java
@@ -0,0 +1,49 @@
+package WallE;
+
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.MovePilot;
+
+public class Driver {
+
+	public BaseRegulatedMotor mRight;
+	public BaseRegulatedMotor mLeft;
+	public Wheel wR;
+	public Wheel wL;
+	public Wheel[] wheels;
+	public MovePilot pilot;
+	public Chassis chassis;
+	
+	
+	public Driver() {
+		this.mRight = new EV3LargeRegulatedMotor(MotorPort.A);
+		this.mLeft = new EV3LargeRegulatedMotor(MotorPort.B);
+		this.wR=  WheeledChassis.modelWheel(mLeft,60).offset(29);
+		this.wL=  WheeledChassis.modelWheel(mRight,60).offset(-29);;
+		this.wheels= new Wheel[] {wR,wL};
+		this.pilot = new MovePilot(chassis);
+				
+				
+	}
+	
+	public void forward() {
+		pilot.forward();
+	}
+	public void turnLeft() {
+		pilot.rotate(-90);
+	}
+	public void turnRight() {
+		pilot.rotate(90);
+		}
+	public void stop() {
+		pilot.stop();
+	}
+
+	public MovePilot getPilot() {
+		return pilot;
+	}
+}
---
+++ WallE/NodeDetector.java
@@ -0,0 +1,20 @@
+import lejos.robotics.localization.OdometryPoseProvider;
+import lejos.robotics.localization.PoseProvider;
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.navigation.Navigator;
+
+public class NodeDetector  {
+	
+public PoseProvider poseP;
+public Navigator navigator;
+
+Driver driver = new Driver();
+
+public NodeDetector() {
+	MovePilot pilot = driver.getPilot();
+	this.poseP = new OdometryPoseProvider(pilot);
+	this.navigator = new Navigator (pilot.getPilot(),poseP);
+}
+
+
+}
