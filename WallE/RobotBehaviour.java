60ab2337e62b91f01080981e2742bd0cd12ec1ba
ibarhim <46403831+Phospheness@users.noreply.github.com>
2024-03-14 15:32:53 +0000

    Repurposed RobotBehaviour.java
    Edited this to actually run and hold the main methods

--- WallE/RobotBehaviour.java
+++ WallE/RobotBehaviour.java
@@ -1,18 +1,22 @@
 package WallE;
 
-import lejos.hardware.port.MotorPort;
-import lejos.robotics.subsumption.*;
+import lejos.hardware.Button;
 
 public class RobotBehaviour {
-	
-	   public static void main(String [] args) {
-	      Behavior b1 = new RotateBehaviour();
-	      //Behavior b2 = new Rescue();
-	      //Behavior b3 = new BatteryLevel(6.5f);
-	      //Behavior b4 = new emergRobotStop();
-	      Behavior[] bArray = {b1};//, b2, b3, b4};
-	      Arbitrator arby = new Arbitrator(bArray);
-	      arby.go();
-	   }
-	}
+    public static void main(String[] args) {
+        RotateBehaviour rotateBehavior = new RotateBehaviour();
+        rotateBehavior.isRotating = true; // Set the flag to true to start rotation
+        
+        boolean actionCalled = false; // Flag to track whether action has been called
+        
+        // Loop until the center button is pressed
+        while (!Button.ENTER.isDown()) {
+            if (!actionCalled) {
+                rotateBehavior.action(); // Call the action method to rotate
+                actionCalled = true; // Set the flag to true after action is called
+            }
+        }
+        
+        rotateBehavior.stopRotate(); // Stop the rotation when the center button is pressed
+    }
 }
