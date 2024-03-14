1a6ba129395208782b6ef96e1cd273253034b07c
ibarhim <46403831+Phospheness@users.noreply.github.com>
2024-03-14 15:12:02 +0000

    Update RobotBehaviour.java
    changed arby.start() to arby.go()
    
    also temporarily commented out other behaviours

--- WallE/RobotBehaviour.java
+++ WallE/RobotBehaviour.java
@@ -1,19 +1,18 @@
 package WallE;
 
-import lejos.nxt.SensorPort;
 import lejos.hardware.port.MotorPort;
 import lejos.robotics.subsumption.*;
 
 public class RobotBehaviour {
 	
 	   public static void main(String [] args) {
-	      Behavior b1 = new RotateBehaviour(MotorPort.A);
-	      Behavior b2 = new Rescue();
-	      Behavior b3 = new BatteryLevel(6.5f);
-	      Behavior b4 = new emergRobotStop();
-	      Behavior [] bArray = {b1, b2, b3, b4};
+	      Behavior b1 = new RotateBehaviour();
+	      //Behavior b2 = new Rescue();
+	      //Behavior b3 = new BatteryLevel(6.5f);
+	      //Behavior b4 = new emergRobotStop();
+	      Behavior[] bArray = {b1};//, b2, b3, b4};
 	      Arbitrator arby = new Arbitrator(bArray);
-	      arby.start();
+	      arby.go();
 	   }
 	}
 }
