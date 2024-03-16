6a97a0bea312b22f9b6a68428fb5cf82cfab4b5a
Dhara108 <161733359+Dhara108@users.noreply.github.com>
2024-03-16 20:19:11 +0000

    Update implementBehaviour.java

--- WallE/implementBehaviour.java
+++ WallE/implementBehaviour.java
@@ -5,11 +5,11 @@ import lejos.robotics.subsumption.Behavior;
 public class implementBehaviour {
 	
 	   public static void main(String [] args) {
-	      //Behavior b1 = new RotateBehaviour(MotorPort.A);
+	      Behavior b1 = new RotateBehaviour();
 	      Behavior b2 = new Rescue();
 	      Behavior b3 = new BatteryLevel(6.5f);
 	      Behavior b4 = new emergRobotStop();
-	      Behavior [] bArray = { b2, b3, b4};
+	      Behavior [] bArray = {b1, b2, b3, b4};
 	      Arbitrator arby = new Arbitrator(bArray);
 	      arby.start(); // might have to change to .go()
 	   }
