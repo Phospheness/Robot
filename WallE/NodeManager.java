099af1f8e6b1a7ca6ffa67afceec9c90f25b57c2
Laijae <laijae@hotmail.com>
2024-03-19 16:33:47 +0000

    Deleted NodeDetector calss to replace with NodeManager. Made changes to NodeManager to fix it.

--- WallE/NodeManager.java
+++ WallE/NodeManager.java
@@ -1,29 +1,23 @@
 
 
-
+import java.util.ArrayList;
 import lejos.robotics.localization.OdometryPoseProvider;
 import lejos.robotics.localization.PoseProvider;
 import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.navigation.Navigator;
-import java.nio.file.Path;
+
 
 public class NodeManager {
 
 	private PoseProvider poseP;
-	private Navigator navigator;
 	private DFS dfs;
-	private Node previousNode;
+   
 
-	Driver driver;
-	MovePilot pilot;
-
-	public NodeManager() {
+	public NodeManager(DFS dfs, Driver driver) {
 		
-		 pilot = driver.getPilot();
-		this.poseP = new OdometryPoseProvider(driver.getPilot());
-		this.navigator = new Navigator (driver.getPilot(),poseP);
-		this.dfs = new DFS();
-		this.previousNode = null;
+		MovePilot pilot = driver.getPilot();
+		this.poseP = new OdometryPoseProvider(pilot);
+		this.dfs = dfs;
 		
 	}
 
@@ -32,22 +26,24 @@ public class NodeManager {
 	 (I do not know how to implement the direction variable in the constructor) 
 	 The created node is then added to the dfs with dfs.addNode(curNode);
 	 */
-public void getCur() {
+
+	public void createNode() {
 		
-		 float x = poseP.getPose().getX();
-	     float y = poseP.getPose().getY();
-	     
-	     Node curNode = new Node(x,y,);
-	     
-	     dfs.addNode(curNode);
-	     
-	/* Code below is used for connecting nodes. 
-	 * The previous nodee points at the next node and then the current node becomes the previous node 
-	 * */ 
-	 	if(previousNode != null) {
-	 		previousNode.setNext(curNode);
-	 	}
-	 	previousNode = curNode;
+		float x = poseP.getPose().getX();
+		float y = poseP.getPose().getY();
+
+		//need to call the sensors to get the directions that are available from this node 
+		ArrayList<Direction> directions = new ArrayList<Direction>();
+		Node curNode = new Node(x,y, directions);
+		
+		dfs.addNode(curNode);
+
+	}
+
+	public void detectNode(){
+		//this method should get the next node that the robot should go to
+		//it should track its current position and compare it to the node it needs to get to (allow for it to be slightly off)
+		//once at the node it should flag dfs to notify it that it has arrived at the node
 	}
 
 
