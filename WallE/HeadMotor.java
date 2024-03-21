ec09a0ae4c3da38da675fff1c73acd71129d2317
ibarhim <46403831+Phospheness@users.noreply.github.com>
2024-03-21 15:55:56 +0000

    Update HeadMotor.java

--- WallE/HeadMotor.java
+++ WallE/HeadMotor.java
@@ -1,54 +1,97 @@
-package WallE;
+package walle;
 
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.robotics.subsumption.Behavior;
 import lejos.utility.Delay;
 
-public class HeadMotor {
-    private static EV3MediumRegulatedMotor spinMotor = new EV3MediumRegulatedMotor(MotorPort.A);
-    private static boolean isRotating = false;
+public class HeadMotor implements Behavior {
+	private static EV3MediumRegulatedMotor spinMotor = new EV3MediumRegulatedMotor(MotorPort.A);
+    public boolean isRotating = false;
+    private Direction currentDirection = Direction.NORTH;
+
+
+    @Override
+    public boolean takeControl() {
+        return isRotating;
+    }
+
+    @Override
+    public void action() {
+        isRotating = true;
+        rotateSensor(500, 90); // Call rotate with specific delay and speed
+    }
+
+    @Override
+    public void suppress() {
+        stopRotate();
+    }
 
     public void rotateSensor(int delay, int speed) {
-        if (!isRotating) {
-            isRotating = true;
-            String direction = "left";
+	    if (!isRotating) {
+	        isRotating = true;
+	
+	        while (isRotating) {
+	            // Assuming currentDirection starts at NORTH
+	            switch (currentDirection) {
+	                case NORTH:
+	                	
+	                    // Rotate to EAST
+	                    rotateDirection(Direction.EAST, 90, speed);
+	                    Delay.msDelay(delay);
+	                    
+	                    // Reset to NORTH
+	                    rotateDirection(Direction.NORTH, 90, speed);
+	                    Delay.msDelay(delay);
+	                    
+	                    // Rotate to WEST
+	                    rotateDirection(Direction.WEST, 90, speed);
+	                    Delay.msDelay(delay);
+	                    
+	                    // Reset to NORTH again
+	                    rotateDirection(Direction.NORTH, 90, speed);
+	                    break;
+	                    
+	                    
+	                case EAST:
+	                    break;
+	                case WEST:
+	                    break;
+	                case SOUTH:
+	                	break;
+	            }
+	
+	            Delay.msDelay(delay); // Additional delay if needed
+	            // No need to toggle direction here since we're controlling it within the case
+	        }
+	    } else {
+	        LCD.drawString("Motor is already rotating", 2, 2);
+	    }
+    }   
 
-            while (isRotating) {
-                // Rotate left
-                rotateDirection("left", 90, speed);
-                Delay.msDelay(delay);
+    public void rotateDirection(Direction direction, int degrees, int speed) {         
+  	  // Adjust rotation based on target direction         
+      	if (direction == Direction.EAST) {             
+      		spinMotor.setSpeed(speed);             
+      		spinMotor.rotate(degrees, true);          
+      	} else if (direction == Direction.NORTH) {             
+      		spinMotor.setSpeed(speed);             
+      		spinMotor.rotate(-degrees, true);          
+      	} else {             
+      		LCD.drawString("Not a valid direction", 0, 0);         
+      	}          
+      	currentDirection = direction;     
+  		}      
 
-                // Rotate right to original position
-                rotateDirection("right", 180, speed);
-                Delay.msDelay(delay);
-
-                // Toggle direction
-                direction = (direction.equalsIgnoreCase("left")) ? "right" : "left";
-            }
-        } else {
-            LCD.drawString("Motor is already rotating", 2, 2);
-        }
-    }
-
-    public void rotateDirection(String direction, int degrees, int speed) {
-        if (direction.equalsIgnoreCase("left")) {
-            spinMotor.rotate(degrees, true);
-        } else if (direction.equalsIgnoreCase("right")) {
-            spinMotor.rotate(-degrees, true);
-        } else {
-            LCD.drawString("Not a valid direction", 0, 0);
-        }
-    }
-
-    public void stopRotate() {
-        if (isRotating) {
-            isRotating = false;
-            spinMotor.stop();
-        } else {
-            LCD.drawString("Motor is not rotating", 2, 2);
-        }
-    }
+    public void stopRotate() {         
+    	if (isRotating) {             
+    		isRotating = false;             
+    		spinMotor.stop();         
+    	} else {             
+			  LCD.drawString("Motor is not rotating", 2, 2);         
+    	}     
+    }  
 
     public boolean getState() {
         return isRotating;
