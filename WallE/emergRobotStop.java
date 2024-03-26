81af906ba1eb73e8a19f491ea2b6f6de19c3d076
Dhara108 <161733359+Dhara108@users.noreply.github.com>
2024-03-26 12:45:33 +0000

    Update emergRobotStop.java

--- WallE/emergRobotStop.java
+++ WallE/emergRobotStop.java
@@ -2,20 +2,14 @@ package WallE;
 
 import lejos.hardware.Button;
 import lejos.hardware.motor.Motor;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3TouchSensor;
-import lejos.robotics.SampleProvider;
 import lejos.robotics.subsumption.*;
 
 public class emergRobotStop implements Behavior {
-  
-  static EV3TouchSensor touch = new EV3TouchSensor(SensorPort.S3);
-  private boolean suppressed = false;
-  static SampleProvider sampleProvider = touch.getTouchMode();
-  static float touchValue = 0;
+	
+    private boolean suppressed = false;
     
 	public boolean takeControl() {
-        return Button.ESCAPE.isDown() || touchValue == 1;
+        return Button.ESCAPE.isDown();
     }
 	
 	public void suppress() {
@@ -28,13 +22,5 @@ public class emergRobotStop implements Behavior {
         Motor.B.stop(); //wheel2
         Motor.C.stop(); // rotate_head
         Motor.D.stop(); // rescue_claws
-        }
-    
-	public void isSensed() {
-		float[] sample = new float[sampleProvider.sampleSize()];
-        sampleProvider.fetchSample(sample, 0);
-        touchValue = sample[0];
-	}	
+        }	
 }
-
-// should stop robot if escape button is pressed manually or touch sensor is pressed
